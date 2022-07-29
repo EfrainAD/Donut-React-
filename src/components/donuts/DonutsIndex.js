5261ed2a5e26c5f6080c98cff3490bb7d4831c63
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-29 13:28:24 -0400

    Added a loading screen icon

--- src/components/donuts/DonutsIndex.js
+++ src/components/donuts/DonutsIndex.js
@@ -1,6 +1,18 @@
+import { useState } from "react"
+import LoadingScreen from '../shared/LoadingScreen'
+
+
 const DonutsIndex = (props) => {
+     const {donuts, setDonuts} = useState(null)
+
+     if (!donuts){
+          return <p>loading... <LoadingScreen/></p>
+     }
+
      return (
-          <h1>Donut's Index</h1>
+          <>
+               <h1>Donut's Index</h1>
+          </>
      )
 }
 
