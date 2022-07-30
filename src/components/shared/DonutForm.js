04600613cfcceba7d56702da575c4f1492b3c826
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-29 20:11:07 -0400

    Form compoent as been filled in.

--- src/components/shared/DonutForm.js
+++ src/components/shared/DonutForm.js
@@ -1,5 +1,35 @@
+import { Form, Button,  } from 'react-bootstrap'
+ 
 const DonutForm = (props) => {
-     return <p>Here's the donut form</p>
+     return (
+          <Form>
+              <Form.Label htmlFor="name">Name</Form.Label>
+              <Form.Control
+                  placeholder="What is your donut's name?"
+                  name="name"
+                  id="name"
+              />
+              <Form.Label htmlFor="from">From</Form.Label>
+              <Form.Control
+                  placeholder="Where is this donut from?"
+                  name="from"
+                  id="from"
+              />
+              <Form.Label htmlFor="thoughts">Thoughts</Form.Label>
+              <Form.Control
+                  placeholder="What are your thoughts/notes on this donut?"
+                  type="string"
+                  name="thoughts"
+                  id="thoughts"
+              />
+              <Form.Check
+                  label="Have you tried this donut yet?"
+                  name="haveEaten"
+                  defaultChecked={ false  }
+              />
+              <Button type="submit">Submit</Button>
+          </Form>
+      )
  }
  
  export default DonutForm 
