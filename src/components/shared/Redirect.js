048d7847f32860802a76d9c4c5bbef8bba14bd1f
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-08-01 11:02:55 -0400

    Clean up. Found a better way.

--- src/components/shared/Redirect.js
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-import { useNavigate } from "react-router-dom"
-// const navigate = useNavigate()
-
-// const Redirect = (props) => {
-//      navigate(props.path)
-// }
-
-// export default Redirect
